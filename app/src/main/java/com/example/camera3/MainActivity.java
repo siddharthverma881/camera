9a77a7a559a24dbdf204c17f496581acc56f7c0c
siddharthverma881 <siddharthverma881@gmail.com>
2019-06-20 10:32:12 +0530

    version - 1.1.0

--- app/src/main/java/com/example/camera3/MainActivity.java
+++ app/src/main/java/com/example/camera3/MainActivity.java
@@ -9,25 +9,29 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button,getImageButton;
     private ImageView imageView;
+    private Uri mUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getImageButton = findViewById(R.id.btn_get);
         button = findViewById(R.id.btn);
         imageView = findViewById(R.id.image_view);
 
@@ -35,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+            }
+        });
+
+        getImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,2);
             }
         });
     }
@@ -45,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
             setPic();
             galleryAddPic();
 
+        }
+        if(resultCode == 2 && requestCode == RESULT_OK){
+            mUri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mUri);
+                imageView.setImageBitmap(bitmap);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
